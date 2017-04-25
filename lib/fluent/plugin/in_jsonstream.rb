cbe8d373eb111a0beec5825131f38bf4d87a5461
Daniel Malon <daniel.malon@me.com>
2017-04-25 13:31:43 +0100

    Recreate the stream parser on parse failures

--- lib/fluent/plugin/in_jsonstream.rb
+++ lib/fluent/plugin/in_jsonstream.rb
@@ -32,22 +32,32 @@ module Fluent::Plugin
       super
 
       server_create_connection(:in_jsonstream_server, @port, bind: @bind) do |conn|
-        parser = Yajl::Parser.new(:symbolize_keys => false)
-        parser.on_parse_complete = lambda { |record|
-          tag = extract_tag_from_record(record)
-          tag ||= @tag
-          time ||= extract_time_from_record(record) || Fluent::EventTime.now
-
-          # Use the recorded event time if available
-          time = record.delete('timestamp').to_i if record.key?('timestamp')
-
-          router.emit(tag, time, record)
-        }
-
+        parser = newParser()
         conn.data do |data|
-          parser << data
+          begin
+            parser << data
+          rescue Yajl::ParseError
+            parser = newParser()
+          end
         end
       end
+    end
+
+    private
+
+    def newParser
+      parser = Yajl::Parser.new(:symbolize_keys => false)
+      parser.on_parse_complete = lambda { |record|
+        tag = extract_tag_from_record(record)
+        tag ||= @tag
+        time ||= extract_time_from_record(record) || Fluent::EventTime.now
+
+        # Use the recorded event time if available
+        time = record.delete('timestamp').to_i if record.key?('timestamp')
+
+        router.emit(tag, time, record)
+      }
+      parser
     end
   end
 end
